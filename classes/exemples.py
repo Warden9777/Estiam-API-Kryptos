a4d7d57005b3f63afd65d9cbc161b3b4e0a446d7
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-11-08 13:29:16 +0100

    build bugfix
    fixed some errors that prevented the project from running
    
    Co-Authored-By: Warden <135692984+Warden9777@users.noreply.github.com>

--- classes/exemples.py
+++ classes/exemples.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
 from typing import List
+from classes.schema_dto import CryptoCurrency as CCModel
 
 CryptoCurrency = [
-    {"id": "btc", "name": "Bitcoin", "amount": 1.5},
-    {"id": "eth", "name": "Ethereum", "amount": 10.2},
-    {"id": "ltc", "name": "Litecoin", "amount": 50.0},
+    CCModel(id= "btc", name= "Bitcoin", amount= 1.5),
+    CCModel(id= "eth", name= "Ethereum", amount= 10.2),
+    CCModel(id= "ltc", name= "Litecoin", amount= 50.0),
 ]
